d7873306fad1890bf3d75ef785f97133776b1d1e
Devin Setiawan <90437494+DevinRS@users.noreply.github.com>
2022-11-06 11:05:11 -0600

    Added Exercise_3
    Exercise_3: Using CSS to style a webpage (Sample Profile Page)

---
+++ EECS_448_WebProgramming/Exercise_3/script.js
@@ -0,0 +1,19 @@
+function theme1(){
+    document.documentElement.style.setProperty('--bg-primary', '#6D7DC6');
+    document.documentElement.style.setProperty('--bg-secondary', '#402D59');
+}
+
+function theme2(){
+    document.documentElement.style.setProperty('--bg-primary', '#565656');
+    document.documentElement.style.setProperty('--bg-secondary', '#4B403A');
+}
+
+function theme3(){
+    document.documentElement.style.setProperty('--bg-primary', '#F34E4E');
+    document.documentElement.style.setProperty('--bg-secondary', '#3E1414');
+}
+
+function theme4(){
+    document.documentElement.style.setProperty('--bg-primary', '#9CCBC5');
+    document.documentElement.style.setProperty('--bg-secondary', '#23312F');
+}
